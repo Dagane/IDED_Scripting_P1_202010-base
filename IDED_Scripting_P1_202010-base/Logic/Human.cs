74b874b36cf706e12852568373f5f2d9c75fee42
Dagane <dagane8@hotmail.com>
2020-02-27 07:53:57 -0500

    Add files via upload

--- IDED_Scripting_P1_202010-base/Logic/Human.cs
+++ IDED_Scripting_P1_202010-base/Logic/Human.cs
@@ -7,12 +7,47 @@
         public Human(EUnitClass _unitClass, int _atk, int _def, int _spd, int _moveRange, float _potential)
             : base(_unitClass, _atk, _def, _spd, _moveRange)
         {
-            Potential = _potential;
+            
+            if (Potential<0.1 && Potential >0)
+            {
+                Potential = _potential;
+            }
+            
+            
         }
-
+        
         public virtual bool ChangeClass(EUnitClass newClass)
         {
             return false;
+        }
+
+
+        public void Humano(EUnitClass a)
+        {
+            if (a is Human)
+            {
+              
+                
+
+                if ((Defense += Defense * Potential) > 255)
+                {
+                    Defense += Defense * Potential;
+                }
+                else { Attack = 255; }
+
+                if ((Speed += Speed * Potential) > 255)
+                {
+                    Speed += Speed* Potential;
+                }
+                else { Attack = 255; }
+
+                if ((Attack += Attack * Potential) > 255)
+                {
+                    Attack += Attack * Potential;
+                }
+                else { Attack = 255; }
+
+            }
         }
     }
 }
--- IDED_Scripting_P1_202010-base/Logic/Unit.cs
+++ IDED_Scripting_P1_202010-base/Logic/Unit.cs
@@ -2,9 +2,9 @@
 {
     public class Unit
     {
-        public int BaseAtk { get; protected set; }
-        public int BaseDef { get; protected set; }
-        public int BaseSpd { get; protected set; }
+        public float BaseAtk { get; protected set; }
+        public float BaseDef { get; protected set; }
+        public float  BaseSpd { get; protected set; }
 
         public int MoveRange { get; protected set; }
         public int AtkRange { get; protected set; }
@@ -13,9 +13,9 @@
         public float BaseDefAdd { get; protected set; }
         public float BaseSpdAdd { get; protected set; }
 
-        public float Attack { get; }
-        public float Defense { get; }
-        public float Speed { get; }
+        public float Attack { get; protected set; }
+        public float Defense { get; protected set; }
+        public float Speed { get; protected set; }
 
         protected Position CurrentPosition;
 
@@ -28,15 +28,112 @@
             BaseDef = _def;
             BaseSpd = _spd;
             MoveRange = _moveRange;
+
         }
 
         public virtual bool Interact(Unit otherUnit)
         {
+            
             return false;
         }
 
         public virtual bool Interact(Prop prop) => false;
 
         public bool Move(Position targetPosition) => false;
+
+
+        public void UnitCaracteristicas()
+        {
+            if (BaseAtk < 225 && BaseAtk > 0)
+            {
+                BaseAtk = BaseAtk;
+            }
+            if (BaseDef< 225 && BaseDef > 0)
+            {
+                BaseDef = BaseDef;
+            }
+            if (BaseSpd < 225 && BaseSpd > 0)
+            {
+                BaseSpd = BaseSpd;
+            }
+
+
+            if (UnitClass == EUnitClass.Ranger)
+            {
+                BaseAtkAdd = 0.1f;
+                BaseDefAdd = 0.0f;
+                BaseSpdAdd = 0.3f;
+                AtkRange = 3;
+
+            }
+            if (UnitClass == EUnitClass.Squire)
+            {
+                BaseAtkAdd = 0.2f;
+                BaseDefAdd = 0.1f;
+                BaseSpdAdd = 0.0f;
+
+            }
+            if (UnitClass == EUnitClass.Soldier)
+            {
+                BaseAtkAdd = 0.3f;
+                BaseDefAdd = 0.2f;
+                BaseSpdAdd = 0.1f;
+
+            }
+            if (UnitClass == EUnitClass.Mage)
+            {
+                BaseAtkAdd = 0.3f;
+                BaseDefAdd = 0.1f;
+                BaseSpdAdd = -0.1f;
+                AtkRange = 3;
+
+            }
+            if (UnitClass == EUnitClass.Imp)
+            {
+                BaseAtkAdd = 0.1f;
+                BaseDefAdd = 0.0f;
+                BaseSpdAdd = 0.0f;
+
+            }
+            if (UnitClass == EUnitClass.Orc)
+            {
+                BaseAtkAdd = 0.4f;
+                BaseDefAdd = 0.2f;
+                BaseSpdAdd = -0.2f;
+
+            }
+            if (UnitClass == EUnitClass.Dragon)
+            {
+                BaseAtkAdd = 0.5f;
+                BaseDefAdd = 0.3f;
+                BaseSpdAdd = 0.2f;
+                AtkRange = 5;
+
+            }
+
+            if((BaseAtk + (BaseAtk * BaseAtkAdd) < 255))
+            {
+                Attack = 255;
+            }
+            else { Attack = BaseAtk + (BaseAtk * BaseAtkAdd); }
+
+            if ((BaseDef + (BaseDef * BaseDefAdd) < 255))
+            {
+                Defense = 255;
+            }
+            else { Defense = BaseDef + (BaseDef * BaseDefAdd); }
+
+            if ((BaseSpd + (BaseSpd * BaseSpdAdd) < 255))
+            {
+                Speed = 255;
+            }
+            else { Speed = BaseSpd + (BaseSpd * BaseSpdAdd); }
+
+
+        }
+
+
+
+
     }
 }
